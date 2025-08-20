5756121d5830d07d9cdfaa8c36e5d91c953d2a71
Triliam <taisbuenovidotto@gmail.com>
2025-08-20 13:50:34 -0300

    domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/BelonginPK.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable
+public class BelonginPK {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+
+    public BelonginPK(){}
+
+    public BelonginPK(Game game, GameList gameList) {
+        this.game = game;
+        this.gameList = gameList;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BelonginPK that)) return false;
+        return getGame().equals(that.getGame()) && getGameList().equals(that.getGameList());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getGame(), getGameList());
+    }
+}
---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,53 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelonginPK id = new BelonginPK();
+
+    private Integer position;
+
+    public Belonging(){}
+
+    public Belonging(Game game, GameList gameList, Integer position){
+        this.id.setGame(game);
+        this.id.setGameList(gameList);
+        this.position = position;
+    }
+
+    public BelonginPK getId() {
+        return id;
+    }
+
+    public void setId(BelonginPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Belonging belonging)) return false;
+        return getId().equals(belonging.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
